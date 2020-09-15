6e76dac43fd5f703c6f48f04ae1d8700cdf0e31b
MatthewCookUNR <matthewthomascook@yahoo.com>
2020-09-14 20:29:25 -0700

    Shooting split into regular and arrow aim

--- game.js
+++ game.js
@@ -3,15 +3,13 @@ const gameState = {
 }
 
 function preload () {
-	this.load.image('bug1', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_1.png');
-	this.load.image('bug2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_2.png');
-	this.load.image('bug3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_3.png');
 	this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
-	this.load.image('gunArrow', 'assets/other/blue_arrow.png');
+	this.load.image('grassTile', 'assets/platforms/grassTile.png')
+	this.load.image('blasterArrow', 'assets/other/blue_arrow.png');
 	this.load.image('pinkBeam', 'assets/other/pink_beam.png');
 	this.load.audio('playerBlasterSound', 'assets/sounds/player_blaster_sound.mp3');
-	this.load.spritesheet('codeyIdle', 'assets/spritesheets/player/Gunner_Blue_Idle.png', { frameWidth: 48, frameHeight: 48 });
-	this.load.spritesheet('codeyRun', 'assets//spritesheets/player/Gunner_Blue_Run.png', { frameWidth: 48, frameHeight: 48 });
+	this.load.spritesheet('playerIdle', 'assets/spritesheets/player/Gunner_Blue_Idle.png', { frameWidth: 48, frameHeight: 48 });
+	this.load.spritesheet('playerRun', 'assets/spritesheets/player/Gunner_Blue_Run.png', { frameWidth: 48, frameHeight: 48 });
 	this.load.spritesheet('skeletonIdle', 'assets/spritesheets/enemies/skeleton/Idle.png', { frameWidth: 150, frameHeight: 150 })
 	this.load.spritesheet('skeletonWalk', 'assets/spritesheets/enemies/skeleton/Walk.png', { frameWidth: 150, frameHeight: 150 })
 
@@ -20,20 +18,22 @@ function preload () {
 //Initializes player and other objects that are there at start
 //of the game
 function create () {
-
-	const platforms = this.physics.add.staticGroup();
+	gameState.platforms = this.physics.add.staticGroup();
  
-	platforms.create(320, 350, 'platform').setScale(2, 0.5).refreshBody();
+	gameState.platforms.create(320, 350, 'platform').setScale(2, 0.5).refreshBody();
+	//Create Scene 1 Platforms
+	createCombinedPlatform(gameState.platforms, 125, 280, 'grassTile', 0.8, 0.8, 4);
+	createCombinedPlatform(gameState.platforms, 400, 280, 'grassTile', 0.8, 0.8, 4);
 
 	gameState.scoreText = this.add.text(320, 340, 'Score: 0', { fontSize: '15px', fill: '#000' })
 	gameState.clockReady = false;
 
-	gameState.player = this.physics.add.sprite(25, 300, 'codeyIdle').setScale(1).setGravityY(150);
+	gameState.player = this.physics.add.sprite(25, 300, 'playerIdle').setScale(1).setSize(35, 35, true).setGravityY(200);
 	gameState.player.jumbReady = true;
 	
 	gameState.player.setCollideWorldBounds(true);
 
-	this.physics.add.collider(gameState.player, platforms, function () {
+	this.physics.add.collider(gameState.player, gameState.platforms, function () {
 		gameState.player.jumbReady = true;
 	});
 
@@ -50,7 +50,7 @@ function create () {
 			const xCoord = between(200,640);
 			gameState.enemies.create(xCoord, -40, 'skeletonIdle')
 			.setCollideWorldBounds(true)
-			.setSize(50, 50, true);
+			.setSize(35, 35, true);
 		}
 	}
 
@@ -59,14 +59,14 @@ function create () {
 	//New Code Character Animations
 	this.anims.create({
 		key: 'playerIdle',
-		frames: this.anims.generateFrameNumbers('codeyIdle', { start: 1, end: 5 }),
+		frames: this.anims.generateFrameNumbers('playerIdle', { start: 1, end: 5 }),
 		frameRate: 5,
 		repeat: -1
 	});
 
 	this.anims.create({
 		key: 'playerRun',
-		frames: this.anims.generateFrameNumbers('codeyRun', { start: 1, end: 5 }),
+		frames: this.anims.generateFrameNumbers('playerRun', { start: 1, end: 5 }),
 		frameRate: 5,
 		repeat: -1
 	});
@@ -79,7 +79,7 @@ function create () {
 	})
 
 	//Colliders
-	this.physics.add.collider(gameState.enemies, platforms);
+	this.physics.add.collider(gameState.enemies, gameState.platforms);
 
 }
 
@@ -99,7 +99,8 @@ function update () {
 	if(!gameState.clockReady) {
 		gameState.gameClock = new Phaser.Time.Clock(this);
 		gameState.gameClock = this.time;
-		gameState.lastShotTime = gameState.gameClock.now;
+		gameState.arrowClockSnapshot = gameState.gameClock.now;
+		gameState.blasterClockSnapshot = gameState.gameClock.now;
 		gameState.clockReady = true;
 	}
 
@@ -112,10 +113,10 @@ function update () {
 				if(!gameState.isAiming) {
 					gameState.player.anims.play('playerRun', true);
 				}
-				if(gameState.gunArrow && !gameState.player.flipX) {
-					let flipAngle = flipRotationAlongXAxis(gameState.gunArrow.angle);
-					Phaser.Actions.RotateAround([gameState.gunArrow], gameState.player, Phaser.Math.DegToRad(flipAngle));
-					gameState.gunArrow.reverse = !gameState.gunArrow.reverse;
+				if(gameState.blasterArrow && !gameState.player.flipX) {
+					let flipAngle = flipRotationAlongXAxis(gameState.blasterArrow.angle);
+					Phaser.Actions.RotateAround([gameState.blasterArrow], gameState.player, Phaser.Math.DegToRad(flipAngle));
+					gameState.blasterArrow.reverse = !gameState.blasterArrow.reverse;
 				}
 				gameState.player.flipX = true;
 			} 
@@ -125,10 +126,10 @@ function update () {
 				if(!gameState.isAiming) {
 					gameState.player.anims.play('playerRun', true);
 				}
-				if(gameState.gunArrow && gameState.player.flipX) {
-					let flipAngle = flipRotationAlongXAxis(gameState.gunArrow.angle);
-					Phaser.Actions.RotateAround([gameState.gunArrow], gameState.player, Phaser.Math.DegToRad(flipAngle));
-					gameState.gunArrow.reverse = !gameState.gunArrow.reverse;
+				if(gameState.blasterArrow && gameState.player.flipX) {
+					let flipAngle = flipRotationAlongXAxis(gameState.blasterArrow.angle);
+					Phaser.Actions.RotateAround([gameState.blasterArrow], gameState.player, Phaser.Math.DegToRad(flipAngle));
+					gameState.blasterArrow.reverse = !gameState.blasterArrow.reverse;
 				}
 				gameState.player.flipX = false;
 			}
@@ -140,84 +141,98 @@ function update () {
 				}
 			}
 	
-			//Jumping
+			//Press W: Jumping
 			if(this.cursors.up.isDown && gameState.player.jumbReady) {
 				gameState.player.jumbReady = false;
-				gameState.player.setVelocityY(-200);
+				gameState.player.setVelocityY(-250);
 			}
 
-	//Press Spacebar: Create Arrow
-	if(this.cursors.spacebar.isDown && !gameState.isAiming) {
-		if(cooldownReady(gameState.lastShotTime, gameState.gameClock.now, 2000)) {
+	//Press E: Create Arrow
+	if(this.cursors.eKey.isDown && !gameState.isAiming) {
+		if(cooldownReady(gameState.arrowClockSnapshot, gameState.gameClock.now, 2000)) {
 				gameState.player.setVelocityX(0);
-				gameState.player.anims.stop();
+				gameState.player.anims.pause();
 				this.physics.pause();
 				gameState.isAiming = true;
 				if(gameState.player.flipX) {
-				gameState.gunArrow = this.add.image(gameState.player.x - 60, gameState.player.y, 'gunArrow').setScale(0.8);
-				gameState.gunArrow.reverse = false;
+				gameState.blasterArrow = this.add.image(gameState.player.x - 60, gameState.player.y, 'blasterArrow').setScale(0.8);
+				gameState.blasterArrow.reverse = false;
 			}
 			else {
-				gameState.gunArrow = this.add.image(gameState.player.x + 60, gameState.player.y, 'gunArrow').setScale(0.8);
-				gameState.gunArrow.reverse = true;
+				gameState.blasterArrow = this.add.image(gameState.player.x + 60, gameState.player.y, 'blasterArrow').setScale(0.8);
+				gameState.blasterArrow.reverse = true;
 			}
-			gameState.lastShotTime = gameState.gameClock.now;
+			gameState.arrowClockSnapshot = gameState.gameClock.now;
 		}
 	}
 
+	//Press Spacebar: Fire Blaster
+	if(this.cursors.spacebar.isDown
+		 && !gameState.isAiming
+		 && cooldownReady(gameState.blasterClockSnapshot, gameState.gameClock.now, 500)) {
+			let projectile = createProjectile(this);
+			if(gameState.player.flipX) {
+				projectile.angle = 180;
+			}
+			else {
+				projectile.angle = 0;
+			}
+			gameState.blasterClockSnapshot = gameState.gameClock.now;
+			gameState.projectiles.add(projectile);
+			this.sound.play('playerBlasterSound');
+	} 
+
 
 	//Handles gun aiming arrow movement
-	if(gameState.gunArrow) {
+	if(gameState.blasterArrow) {
 		//Calculates direction pointing from player
-		var angleDeg = Math.atan2(gameState.gunArrow.y - gameState.player.y, gameState.gunArrow.x - gameState.player.x) * 180 / Math.PI;
-		gameState.gunArrow.angle = angleDeg+180;
+		var angleDeg = Math.atan2(gameState.blasterArrow.y - gameState.player.y, gameState.blasterArrow.x - gameState.player.x) * 180 / Math.PI;
+		gameState.blasterArrow.angle = angleDeg+180;
 		//Right Pointing Arrow
 		if(!gameState.player.flipX) {
-			if(gameState.gunArrow.reverse) {
-				Phaser.Actions.RotateAroundDistance([gameState.gunArrow], gameState.player, +0.04, 75);
+			if(gameState.blasterArrow.reverse) {
+				Phaser.Actions.RotateAroundDistance([gameState.blasterArrow], gameState.player, +0.04, 75);
 				if(shouldArrowReverse(Math.ceil(angleDeg), Phaser.Math.Angle.WrapDegrees(75))) {
-					gameState.gunArrow.reverse = false;
+					gameState.blasterArrow.reverse = false;
 				}
 			}	
 			else {
-				Phaser.Actions.RotateAroundDistance([gameState.gunArrow], gameState.player, -0.04, 75);
+				Phaser.Actions.RotateAroundDistance([gameState.blasterArrow], gameState.player, -0.04, 75);
 				if(shouldArrowReverse(Math.floor(angleDeg), Phaser.Math.Angle.WrapDegrees(-75))) {
-					gameState.gunArrow.reverse = true;
+					gameState.blasterArrow.reverse = true;
 				}
 			}
 		}
 		//Left Pointing Arrow
 		else {
-			if(gameState.gunArrow.reverse) {
-				Phaser.Actions.RotateAroundDistance([gameState.gunArrow], gameState.player, +0.04, 75);
+			if(gameState.blasterArrow.reverse) {
+				Phaser.Actions.RotateAroundDistance([gameState.blasterArrow], gameState.player, +0.04, 75);
 				if(shouldArrowReverse(Math.ceil(angleDeg), Phaser.Math.Angle.WrapDegrees(-105))) {
-					gameState.gunArrow.reverse = false;
+					gameState.blasterArrow.reverse = false;
 				}
 			}	
 			else {
-				Phaser.Actions.RotateAroundDistance([gameState.gunArrow], gameState.player, -0.04, 75);
+				Phaser.Actions.RotateAroundDistance([gameState.blasterArrow], gameState.player, -0.04, 75);
 				if(shouldArrowReverse(Math.floor(angleDeg), Phaser.Math.Angle.WrapDegrees(105))) {
-					gameState.gunArrow.reverse = true;
+					gameState.blasterArrow.reverse = true;
 				}
 			}
 		}
-		//console.log(gameState.gunArrow.angle);
+		//console.log(gameState.blasterArrow.angle);
 
 
 		//Handles if the spacebar is pressed with aiming arrow 
 		//which ----> shoots the gun
-		if(this.cursors.spacebar.isDown && cooldownReady(gameState.lastShotTime, gameState.gameClock.now, 1000  )) {
-			let projectile = this.physics.add.sprite(gameState.player.x, gameState.player.y, 'pinkBeam').setScale(0.7);
-			//Colliders
-			this.physics.add.collider(projectile, gameState.enemies, (enemy, projectile) => {
-				enemy.destroy();
-				projectile.destroy();
-			});
+		if(this.cursors.spacebar.isDown 
+			&& gameState.isAiming
+			&& cooldownReady(gameState.arrowClockSnapshot, gameState.gameClock.now, 1000  )) {
+			let projectile = createProjectile(this);
 			projectile.angle = angleDeg;
 			gameState.projectiles.add(projectile);
-			gameState.lastShotTime = gameState.gameClock.now;
-			gameState.gunArrow.destroy();
-			gameState.gunArrow = null;
+			gameState.arrowClockSnapshot = gameState.gameClock.now;
+			gameState.blasterClockSnapshot = gameState.gameClock.now;
+			gameState.blasterArrow.destroy();
+			gameState.blasterArrow = null;
 			gameState.isAiming = false;
 			this.sound.play('playerBlasterSound');
 			this.physics.resume();
@@ -239,7 +254,7 @@ function update () {
 		let enemies = gameState.enemies.getChildren();
 		for(let i = 0; i < enemies.length; i++) {
 			enemies[i].setVelocityX(0);
-			if(!gameState.gunArrow) {
+			if(!gameState.blasterArrow) {
 				enemies[i].anims.play('skeletonIdle', true);
 			}
 			else {
@@ -294,11 +309,11 @@ function between(min, max) {
 
 
 //Functions returns true if cooldown is ready
-function cooldownReady(lastShotTime, currentClock, cooldownTime) {
-	//console.log("Last shot: " + lastShotTime);
+function cooldownReady(clockSnapshot, currentClock, cooldownTime) {
+	//console.log("Last shot: " + clockSnapshot);
 	//console.log("Current Clock: " + currentClock);
-	//console.log("Difference: " + (currentClock - lastShotTime));
-	if((currentClock - lastShotTime) < cooldownTime)
+	//console.log("Difference: " + (currentClock - clockSnapshot));
+	if((currentClock - clockSnapshot) < cooldownTime)
 	{
 		return false;
 	}
@@ -306,18 +321,46 @@ function cooldownReady(lastShotTime, currentClock, cooldownTime) {
 	 	return true;
 }
 
+//Function creates a platform by repeating the tile a given number of times to the right of x,y input
+function createCombinedPlatform(platformGroup, x, y, type, scaleX, scaleY, numberPlatforms) {
+	for(let i = 0; i < numberPlatforms; i++) {
+		platformGroup.create(x + i*35, y, type).setScale(scaleX, scaleY).refreshBody();
+	}
+}
+
+//Function creates projectile object with given info
+//Note: Takes in scene (this) in order to set physics/collider
+function createProjectile(scene) {
+	let projectile;
+	if(gameState.player.flipX) {
+		projectile = scene.physics.add.sprite(gameState.player.x - 30, gameState.player.y, 'pinkBeam').setScale(0.7);
+	}
+	else {
+		projectile = scene.physics.add.sprite(gameState.player.x + 30, gameState.player.y, 'pinkBeam').setScale(0.7);
+	}
+	//Colliders
+	scene.physics.add.collider(projectile, gameState.enemies, (enemy, projectile) => {
+		enemy.destroy();
+		projectile.destroy();
+	});
+	scene.physics.add.collider(projectile, gameState.platforms, (projectile) => {
+		projectile.destroy();
+	});
+	return projectile;
+}
+
 //Game Config
 const config = {
   type: Phaser.AUTO,
   width: 640,
-	height: 360,
+	height: 640,
 	backgroundColor: "ffffff",
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: {y: 200},
 			enableBody: true,
-			debug: true,
+			debug: false,
 		}
 	},
   scene: {
